7d298e0a29e84bfdde32c55eb25efbcc4d46894e
Crowdbotics <team@crowdbotics.com>
2021-08-31 19:28:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "autumn_dust_30232Identifier",
+  url: "https://autumn_dust_30232Identifier.botics.co",
+  api: "https://autumn_dust_30232Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
